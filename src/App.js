dd835cbbbfdb2af07335949edb6ba1a2eb0636db
Shanmugasundaram Raja <100921063+ShanmugasundaramRaja@users.noreply.github.com>
2022-06-29 17:12:20 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import "./App.css";
+import CustomNavbar from "./Component/NavBar";
+import Header from "./Component/Header";
+import RowOfMovies from "./Component/RowOfMovies";
+import ShowDetail from "./Component/ShowDetail";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <CustomNavbar />
+      <Header />
+      <Route
+        path="/"
+        exact
+        render={(routerProps) => (
+          <RowOfMovies {...routerProps} title="Star Wars" />
+        )}
+      />
+      <Route
+        path="/"
+        exact
+        render={(routerProps) => (
+          <RowOfMovies {...routerProps} title="Star Wars" />
+        )}
+      />
+      <Route
+        path="/"
+        exact
+        render={(routerProps) => (
+          <RowOfMovies {...routerProps} title="Star Wars" />
+        )}
+      />
+      <Route
+        path="/details/:movieID"
+        render={(routerProps) => <ShowDetail {...routerProps} title="hello" />}
+      />
+    </Router>
+  );
+}
+
+export default App;
